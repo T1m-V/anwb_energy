f9524a59d2cb39629dda17f6e3aa300caae6ff86
TimV <tim.vogel@live.nl>
2022-12-26 13:25:00 +0100

    Taking a different approach

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
+import json
+import os
+import urllib.request
+from datetime import date, timedelta
 
-from func_file_ut import save_file
+import numpy as np
 
-url = "https://energie.anwb.nl/actuele-tarieven"
-print("URL : %s" % url)
 
-driver = webdriver.Chrome()
-driver.get(url)
+def energyzero_api_loader_today_tomorrow(today, usage):
+    urlbase = "https://api.energyzero.nl/v1/energyprices?"
 
-html = driver.page_source
-soup = BeautifulSoup(html)
+    # Define date range
+    yesterday = today - timedelta(days=1)
+    fromDate = str(yesterday) + "T23%3A00%3A00.000Z"
+    tillDate = str(today) + "T22%3A00%3A00.000Z"
 
-print(soup)
-save_file("html.txt", soup.prettify())
+    # Constants
+    interval = 4
+    inclBtw = "True"
+
+    # Construct URL
+    url = (
+        urlbase
+        + "fromDate="
+        + fromDate
+        + "&tillDate="
+        + tillDate
+        + "&interval="
+        + str(interval)
+        + "&usageType="
+        + str(usage)
+        + "&inclBtw="
+        + inclBtw
+    )
+
+    # Get data from URL
+    with urllib.request.urlopen(url) as urlopener:
+        data = json.load(urlopener)
+
+    return data
+
+
+def get_raw_energy_prices(today, sort):
+    # Define paths
+    libPath = f"./backlog_{sort}/{str(today)}{sort}.npy"
+
+    # Check if we already have the data otherwise load it from API
+    if os.path.exists(libPath):
+        data = np.load(libPath, allow_pickle=True).item()
+    else:
+        if sort == "gas":
+            usage = 3
+        elif sort == "electricity":
+            usage = 1
+        else:
+            print("Do not know how to handle the energy prices of sort:", sort)
+            return None
+        data = energyzero_api_loader_today_tomorrow(today, usage)
+
+    # Check if the data already exists, otherwise return None and try later again.
+    if len(data["Prices"]) != 24:
+        return None
+
+    # Save data for later use
+    np.save(libPath, data)
+
+    return data
+
+
+def get_price_curve(data):
+    # If not available, display 0.
+    if not data:
+        return [0] * 24
+
+    # Collect data and add to hourly curve.
+    todayPrices = data["Prices"]
+    curve = []
+    for i in range(len(todayPrices)):
+        curve.append(todayPrices[i]["price"])
+    return curve
+
+
+if __name__ == "__main__":
+    # Specify the day we want the energy prices.
+    today = date.today()
+    wantedDay = today - timedelta(days=4)
+
+    # Collect the data
+    dataGas = get_raw_energy_prices(wantedDay, "gas")
+    dataElectricity = get_raw_energy_prices(wantedDay, "electricity")
+
+    gasCurve = get_price_curve(dataGas)
+    electricityCurve = get_price_curve(dataElectricity)
+
+    # Print the data
+    print("The electricity prices for", wantedDay, "are: ", electricityCurve)
+    print("The gas prices for", wantedDay, "are: ", gasCurve)
